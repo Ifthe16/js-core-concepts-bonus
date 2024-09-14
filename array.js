bdac75048a275ef0449203868de1ad2145c65b05
Ifthe16 <habiburrahmanif975@gmail.com>
2024-09-14 17:26:29 +0600

    added the object, array and arguments file of javascript

---
+++ array.js
@@ -0,0 +1,17 @@
+const products = [
+    { name: 'phone', price: 12000 },
+    { name: 'laptop', price: 32000 },
+]
+products[0];
+products[1];
+console.log(products[0].name);
+console.log(products[1]);
+
+
+//const products2 = [15,56,87]
+const products2 = {
+    '0': 15,
+    '1': 56,
+    '2': 87
+}
+console.log(products2[0]);
